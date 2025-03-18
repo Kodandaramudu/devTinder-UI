627938a90f23320f7b6cde405760483aae2ac16d
Ram Jammula <ramchowdary0308@gmail.com>
2025-03-18 18:26:15 -0400

    Edit Profile is completed

---
+++ src/components/EditProfile.jsx
@@ -0,0 +1,178 @@
+import React from "react";
+import { useState } from "react";
+import UserCard from "./UserCard";
+import axios from "axios";
+import { BASE_URL } from "../utils/constant";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+
+const EditProfile = ({ user }) => {
+  const [firstName, setFirstName] = useState(user?.firstName);
+  const [lastName, setLastName] = useState(user?.lastName);
+  const [age, setAge] = useState(user?.age);
+  const [gender, setGender] = useState("Gender");
+  const [about, setAbout] = useState(user?.about);
+  const [photoUrl, setPhotoUrl] = useState(user?.photoUrl);
+  const [preview, setPreview] = useState(user?.photoUrl);
+  const [error, setError] = useState("");
+  const [showOtherGender,setShowOtherGender] = useState(false);
+  const [toast, setToast] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const handleClick = (gender) => {
+    if(gender === "others"){
+      setShowOtherGender(true);
+     }
+    setGender(gender);
+    }
+  
+  const saveUserData = async () => {
+    try {
+      const res = await axios.put(
+        BASE_URL + "/profile/edit",
+        {
+          firstName,
+          lastName,
+          photoUrl,
+          age,
+          gender,
+          about,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(addUser(res?.data?.data));
+      setToast(true);
+      setTimeout(() => {
+        setToast(false);
+      }, 2000);
+    } catch (err) {
+      setError(err?.response?.data?.message);
+    }
+  };
+
+  return (
+    user && (
+      <div className="grid my-4 place-items-center text-black">
+        {toast && (
+          <div className="alert alert-success">
+            <span>Profile Saved Successfully</span>
+          </div>
+        )}
+        <div className="grid grid-cols-2 gap-4">
+          <div className="w-full h-[500px] rounded-2xl flex items-center justify-center ">
+            <div className="space-y-4">
+              <fieldset className="fieldset">
+                <input
+                  type="text"
+                  className="input"
+                  placeholder="First Name"
+                  value={firstName}
+                  onChange={(e) => {
+                    setFirstName(e.target.value);
+                  }}
+                />
+              </fieldset>
+              <fieldset className="fieldset">
+                <input
+                  type="text"
+                  className="input"
+                  placeholder="Last Name"
+                  value={lastName}
+                  onChange={(e) => {
+                    setLastName(e.target.value);
+                  }}
+                />
+              </fieldset>
+              <div className="join w-[95%]">
+                <input
+                  type="text"
+                  className="input"
+                  placeholder="photo"
+                  value={photoUrl}
+                  onChange={(e) => {
+                    setPhotoUrl(e.target.value);
+                  }}
+                />
+              </div>
+              <fieldset className="fieldset">
+                <input
+                  type="text"
+                  className="input"
+                  placeholder="Age"
+                  value={age}
+                  onChange={(e) => {
+                    setAge(e.target.value);
+                  }}
+                />
+              </fieldset>
+              <fieldset className="fieldset">
+                <div className="dropdown dropdown-hover">
+                  <div tabIndex={0} role="button" className="btn w-[90%] justify-start">
+                    {gender}
+                  </div>
+                  <ul
+                    tabIndex={0}
+                    className="dropdown-content menu bg-base-100 rounded-box z-1 w-[90%] p-2 shadow-sm justify-start"
+                  >
+                    <li className="justify-start">
+                      <a onClick={()=>handleClick("male")}>Male</a>
+                    </li>
+                    <li className="justify-start">
+                      <a onClick={()=>handleClick("female")}>Female</a>
+                    </li>
+                    <li className="justify-start">
+                      <a onClick={()=>handleClick("others")}>Others</a>
+                    </li>
+                  </ul>
+                </div>
+              </fieldset>
+              {showOtherGender && <fieldset className="fieldset">
+                <input
+                  type="text"
+                  className="input"
+                  placeholder="Enter Your Gender"
+                  value={gender}
+                  onChange={(e) => {
+                    setAge(e.target.value);
+                  }}
+                />
+              </fieldset>}
+              <fieldset className="fieldset">
+                <textarea
+                  rows={2}
+                  className="textarea"
+                  placeholder="about"
+                  value={about}
+                  onChange={(e) => {
+                    setAbout(e.target.value);
+                  }}
+                />
+              </fieldset>
+              {error && (
+                <div className="alert alert-error w-[90%]">
+                  <span>{error}</span>
+                </div>
+              )}
+              <button
+                className="btn btn-soft mb-0 w-[150px] mx-24 hover:shadow-gray-500"
+                onClick={saveUserData}
+              >
+                Save Profile
+              </button>
+            </div>
+          </div>
+          <div className="w-full h-[500px] rounded-2xl flex items-center justify-center">
+            <UserCard
+              userData={{ firstName, lastName, photoUrl, age, gender, about }}
+            />
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default EditProfile;
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -4,22 +4,29 @@ const UserCard = ({ userData }) => {
   const { firstName, lastName, photoUrl, about, gender, age, skills } =
     userData;
 
-  return (
+  return ( 
     <div>
-      <div className="card bg-[#0a192f] text-gray-300 w-96 shadow-sm">
-        <figure>
-          <img className="p-4 rounded-2xl" src={photoUrl} alt="Shoes" />
-        </figure>
+      <div className="card w-96 shadow-sm">
+        <div className="avatar justify-center">
+          <div className="ring-primary ring-offset-base-100 w-52 h-52 rounded-full ring ring-offset-2">
+            <img src={photoUrl} />
+          </div>
+        </div>
         <div className="card-body ">
           <h2 className="card-title inline-flex">
             {firstName + " " + lastName}
           </h2>
+          <div className="space-x-5">
+            <span>{age}</span>
+            <span>{gender}</span>
+          </div>
+
           <div>
-            <p>{about}</p>
+            <p className="line-clamp-3">{about}</p>
           </div>
           <div className="card-actions justify-center">
-            <button className="btn btn-primary">Ignore</button>
-            <button className="btn btn-secondary">Interest</button>
+            <button className="btn btn-soft btn-error">Ignore</button>
+            <button className="btn btn-soft btn-success">Interest</button>
           </div>
         </div>
       </div>
